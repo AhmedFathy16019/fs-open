117ac00c2c7958ef60a5999b8801fd55661423b7
AhmedFathy16019 <ahmed_fathi@aucegypt.edu>
2024-07-19 03:33:05 +0300

    starting exercises 8.8-8.12

---
+++ part8/phone-book-apollo-client/src/components/persons.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { FIND_PERSON } from "../queries";
+import Person from "./person";
+
+
+
+const Persons = ({ persons }) => {
+    const [nameToSearch, setNameToSearch] = useState(null);
+    
+    const result = useQuery(FIND_PERSON, {
+        variables: { nameToSearch },
+        skip: !nameToSearch,
+    });
+
+    if(nameToSearch && result.data) {
+        return (
+            <Person
+                person={result.data.findPerson}
+                onClose={() => setNameToSearch(null)}
+            />
+
+        )
+    }
+
+    return (
+        <div>
+            <h2>Persons</h2>
+            { persons.map(p => {
+                return (
+                    <div key={p.id}>
+                        {p.name} {p.phone}
+                        <button onClick={() => setNameToSearch(p.name)}>
+                            show address
+                        </button>
+                    </div>
+                )
+            }) }
+        </div>
+    )
+}
+
+export default Persons;
