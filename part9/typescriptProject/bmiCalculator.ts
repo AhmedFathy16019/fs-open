9d9096f0005de31e1173f5ecb9b0de12a9936466
AhmedFathy16019 <ahmed_fathi@aucegypt.edu>
2024-09-28 18:11:49 +0300

    added exercises 9.1-9.3

---
+++ part9/typescriptProject/bmiCalculator.ts
@@ -0,0 +1,55 @@
+import { isNotNumber } from "./utils";
+
+interface BmiValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]) : BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (isNotNumber(args[2]) || isNotNumber(args[3])) {
+        throw new Error('Provided values were not numbers!');
+    }
+
+    return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+    }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / Math.pow(height / 100, 2);
+    
+    if (bmi < 16) {
+        return 'Underweight (Severe thinness)';
+    } else if (bmi >= 16 && bmi < 17) {
+        return 'Underweight (Moderate thinness)';
+    } else if (bmi >= 17 && bmi < 18.5) {
+        return 'Underweight (Mild thinness)';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)';
+    } else if (bmi >= 25 && bmi < 30) {
+        return 'Overweight (Pre-obese)';
+    } else if (bmi >= 30 && bmi < 35) {
+        return 'Obese (Class I)';
+    } else if (bmi >= 35 && bmi < 40) {
+        return 'Obese (Class II)';
+    } else {
+        return 'Obese (Class III)';
+    }
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened. ';
+
+    if (error instanceof Error) {
+        errorMessage += 'Error: ' + error.message;
+    }
+
+    console.log(errorMessage);
+}
---
+++ part9/typescriptProject/exerciseCalculator.ts
@@ -0,0 +1,67 @@
+import { isNotNumber } from "./utils";
+
+interface exerciseCalculatorResults {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface exerciseValues {
+    target: number;
+    dailyExerciseHours: number[];
+}
+
+const parseArguments = (args: string[]): exerciseValues => {
+    const target = args[2];
+
+    const dailyExerciseHours = args.slice(3).map(hours => hours);
+    const isDailyHoursValid = dailyExerciseHours.every(hours => !isNotNumber(hours));
+    
+    if (!isNotNumber(target) && isDailyHoursValid) {
+        return {
+            target: Number(target),
+            dailyExerciseHours: dailyExerciseHours.map(hours => Number(hours))
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateExercise = (dailyExerciseHours: number[], target: number): exerciseCalculatorResults => {
+    const trainingDays = dailyExerciseHours.filter(hours => hours > 0).length;
+    const average = dailyExerciseHours.reduce((a, b) => a + b, 0) / dailyExerciseHours.length;
+    const rating = (average >= target) 
+        ? 3 
+        : (average >= target * 0.8) ? 2 : 1;
+    const ratingDescription = (rating === 3) 
+        ? 'Great job!' 
+        : (rating === 2) ? 'Not too bad but could be better' 
+        : 'You should exercise more';
+
+    return {
+        periodLength: dailyExerciseHours.length,
+        trainingDays,
+        success: average >= target,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+try {
+    const { target, dailyExerciseHours } = parseArguments(process.argv);
+    console.log(calculateExercise(dailyExerciseHours, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened. ';
+
+    if (error instanceof Error) {
+        errorMessage += 'Error: ' + error.message;
+    }
+
+    console.log(errorMessage);
+}
