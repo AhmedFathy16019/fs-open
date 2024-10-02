083cb2b65453990aeb3de00c7b476fce3ad6c02b
AhmedFathy16019 <ahmed_fathi@aucegypt.edu>
2024-10-03 00:17:44 +0300

    starting 9.14

--- part9/ilari-flight-diaries/src/routes/diaries.ts
+++ part9/ilari-flight-diaries/src/routes/diaries.ts
@@ -1,7 +1,8 @@
-import express, { Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import diaryService from '../services/diaryService';
-import { NonSensitiveDiaryEntry } from '../types';
-import toNewDiaryEntry from '../utils';
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { z } from 'zod';
+import { NewEntrySchema } from '../utils';
 
 const router = express.Router();
 
@@ -19,22 +20,29 @@ router.get('/:id', (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
     try {
-        const newDiaryEntry = toNewDiaryEntry(req.body);
-    
-        const addedEntry = diaryService.addDiary(newDiaryEntry);
-    
-        res.json(addedEntry);
+        NewEntrySchema.parse(req.body);
+        next();
     } catch (error: unknown) {
-        let errorMessage = "Something went wrong";
-
-        if (error instanceof Error) {
-            errorMessage +=  'Error: ' + error.message;
-        }
-
-        res.status(400).send(errorMessage);
+        next(error);
     }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof z.ZodError) {
+        res.status(400).json({ error: error.issues });
+    } else {
+        next(error);
+    }
+};
+
+router.post('/', newDiaryParser, (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {
+    const addedEntry = diaryService.addDiary(req.body);
+
+    res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
 
 export default router;
